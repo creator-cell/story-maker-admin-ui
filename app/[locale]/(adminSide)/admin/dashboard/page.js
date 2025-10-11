be5903056373966dbf96748877d1ddfe7090c7e9
shivamcumulative <shivamcumulative@gmail.com>
2025-10-11 15:23:01 +0530

    update designg
    add dashboard page and create design

---
+++ app/[locale]/(adminSide)/admin/dashboard/page.js
@@ -0,0 +1,18 @@
+import AnalyticDashboard from "@/app/components/admin/AnalyticDashboard";
+import TranslationProvider from "@/app/components/TranslationProvider";
+import initTranslations from "@/app/i18n";
+const i18nNamespaces = ["common"];
+const Page = async ({ params }) => {
+  const { locale } = await params;
+   const { t, resources } = await initTranslations(locale, i18nNamespaces);
+  return (
+    <TranslationProvider
+      locale={locale}
+      namespaces={i18nNamespaces}
+      resources={resources}
+    >
+      <AnalyticDashboard></AnalyticDashboard>
+    </TranslationProvider>
+  );
+};
+export default Page;
