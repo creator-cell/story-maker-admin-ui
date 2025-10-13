bb4852adcf296ee8cfe3579681fe6b17d62c3bd5
shivamcumulative <shivamcumulative@gmail.com>
2025-10-13 18:20:08 +0530

    update design
    dashboard page design create and template action button visible

--- app/components/admin/AnalyticDashboard.jsx
+++ app/components/admin/AnalyticDashboard.jsx
@@ -1,4 +1,53 @@
-export default function AnalyticDashboard() {
+"use client";
+import $ from 'jquery';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel';
+import { useEffect } from 'react';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+export default function AnalyticDashboard({ lang = "en" }) {
+    const { t } = useTranslation();
+
+    useEffect(() => {
+        const slider = $(".autoplay");
+        if (slider.hasClass("slick-initialized")) {
+            slider.slick("unslick");
+        }
+        slider.slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            arrows: false,
+            dots: true,
+            rtl: lang === "ar",
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    },
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    },
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    },
+                },
+            ],
+        });
+    }, [lang]);
     return (
         <>
             <div id="main_container">
@@ -9,42 +58,42 @@ export default function AnalyticDashboard() {
                                 <div className="row">
                                     <div className="col-lg-12 col-md-12 col-sm-12">
                                         <div className="title_head">
-                                            <h1>Analytic Dashboard</h1>
+                                            <h1>{t("Analytic Dashboard")}</h1>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="analytic-dashboard">
-                                    <div className="row">
+                                    <div className="row dashboard-content" >
                                         <div className="col-lg-4 col-md-4 col-12">
-                                            <div className="box">
+                                            <div className="box justify-content-center align-items-center">
                                                 <div className="icon">
                                                     <img src="/images/active-user.png" alt="active-user" />
                                                 </div>
                                                 <div className="">
-                                                    <p class="">Active Users</p>
-                                                    <span class="">Total active users this month</span>
+                                                    <p >Active Users</p>
+                                                    <span >Total active users this month      </span>
                                                 </div>
                                             </div>
                                         </div>
                                         <div className="col-lg-4 col-md-4 col-12">
-                                            <div className="box">
+                                            <div className="box justify-content-center align-items-center">
                                                 <div className="icon">
                                                     <img src="/images/design-create.png" alt="active-user" />
                                                 </div>
                                                 <div className="">
-                                                    <p class="">Designs Created</p>
-                                                    <span class="">Total designs made by users</span>
+                                                    <p >Designs Created</p>
+                                                    <span >Total designs made by users</span>
                                                 </div>
                                             </div>
                                         </div>
                                         <div className="col-lg-4 col-md-4 col-12">
-                                            <div className="box">
+                                            <div className="box justify-content-center align-items-center">
                                                 <div className="icon">
                                                     <img src="/images/use-template.png" alt="active-user" />
                                                 </div>
                                                 <div className="">
-                                                    <p class="">Templates Used</p>
-                                                    <span class="">Number of templates applied</span>
+                                                    <p >Templates Used</p>
+                                                    <span >Number of templates applied</span>
                                                 </div>
                                             </div>
                                         </div>
@@ -52,25 +101,40 @@ export default function AnalyticDashboard() {
                                 </div>
 
                                 <div className="analytic-dashboard">
-                                    <div className="row">
+                                    <div className="row dashboard-content">
                                         <div className="col-lg-6 col-md-6 col-12">
                                             <div className="box">
-
+                                                <p>User growth over time</p>
                                             </div>
                                         </div>
                                         <div className="col-lg-6 col-md-6 col-12">
                                             <div className="box">
-
+                                                <p>Storage usage</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div className="analytic-dashboard">
+                                <div className="analytic-dashboard" dir={lang === "ar" ? "rtl" : "ltr"}>
                                     <div className="row">
-                                        <div className="col-lg-12 col-md-12 col-12">
-                                            <div className="box">
-                                                <img src="/images/template.png" alt="Template" />
+                                        <div className="col-lg-12 col-md-12 col-12 chart">
+                                            <p>Top used templates and asset </p>
+                                            <div className="slider autoplay">
+                                                <div className='boxes'>
+                                                    <img src="/images/template.jpg" alt="Template" />
+                                                </div>
+                                                <div className='boxes'>
+                                                    <img src="/images/templates.webp" alt="Template" />
+                                                </div>
+                                                <div className='boxes'>
+                                                    <img src="/images/template.jpg" alt="Template" />
+                                                </div>
+                                                <div className='boxes'>
+                                                    <img src="/images/templates.webp" alt="Template" />
+                                                </div>
+                                                <div className='boxes'>
+                                                    <img src="/images/template.jpg" alt="Template" />
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
